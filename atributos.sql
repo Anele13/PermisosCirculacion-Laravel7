30bb282b368f1e2621503c0757956f57321ec3d6
taniaaranda <taniia.a_@hotmail.com>
2020-07-05 18:33:02 -0300

    atributos

---
+++ atributos.sql
@@ -0,0 +1,11 @@
+/*INSERT INTO `laravel`.`requerimientos`
+(`id`,
+`datos_persona`,
+`datos_sitio`,
+`datos_organizacion`)
+VALUES
+(1,
+'{"nombre":"true", "apellido":"true","dni":"true","legajo":"true","email":"true"
+,"sector":"true","superior":"true"}',
+'{"imagen":"true","colorFondo":"true","colorBarra":"true"}',
+'{"dependencia":"true","espacio":"true"}');*/
